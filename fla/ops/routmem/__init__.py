fd80620a798b9a61b20ea59fccce003303067fc9
arshiaafzal <arshiaafzal1378@gmail.com>
2026-01-02 13:35:48 +0100

    Adding ops of routmem which kernels are there

---
+++ fla/ops/routmem/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .chunk import chunk_routmem
+from .fused_recurrent import fused_recurrent_routmem
+
+__all__ = [
+    'chunk_routmem',
+    'fused_recurrent_routmem'
+]
